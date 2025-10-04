d4d952b3122480c2beb2f174479842664d5d138c
Youssef Shawky <youssefshawky691@gmail.com>
2025-10-05 00:42:14 +0300

    Integrate modal progress HUD in AddNoteBottomSheet and update dependencies

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubit/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_form_state.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -14,7 +17,24 @@ class AddNoteBottomSheet extends StatelessWidget {
           right: 16,
           bottom: MediaQuery.of(context).viewInsets.bottom + 16,
         ),
-        child: AddNoteFormState(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            } else if (state is AddNoteFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Failed to add note: ${state.error}')),
+              );
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoading ? true : false,
+              progressIndicator: const CircularProgressIndicator(),
+              child: const AddNoteFormState(),
+            );
+          },
+        ),
       ),
     );
   }
